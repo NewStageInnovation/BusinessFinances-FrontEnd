8b2e09b9f612bf8819b1d79077671c13fca11cc3
Angel Garcia <angel_012912@hotmail.com>
2023-09-19 17:45:31 -0600

    Re-estructuracion de componente tabledata

--- src/Components/TableDataComponent.jsx
+++ src/Components/TableDataComponent.jsx
@@ -6,41 +6,19 @@ import { AiOutlineEdit } from "react-icons/ai";
 function TableDataComponent(props) {
   const data = props.data
     ? props.data
-    : [
-        {
-          fecha: "13/09/2023",
-          concepto: "Pago de empleados",
-          categoria: "Activo",
-          monto: 1000,
-        },
-        {
-          fecha: "13/09/2023",
-          concepto: "Pago de empleados",
-          categoria: "Activo",
-          monto: 1000,
-        },
-        {
-          fecha: "04/09/2023",
-          concepto: "Pago de empleados",
-          categoria: "Activo",
-          monto: 1000,
-        },
-        {
-          fecha: "04/09/2023",
-          concepto: "Pago de empleados",
-          categoria: "Activo",
-          monto: 1000,
-        },
-      ];
+    : [];
 
   return (
     <div className="table-data-container">
       <Table>
         <Table.Head>
-          <Table.TextHeaderCell>Fecha</Table.TextHeaderCell>
-          <Table.TextHeaderCell>Concepto</Table.TextHeaderCell>
-          <Table.TextHeaderCell>Categoría</Table.TextHeaderCell>
-          <Table.TextHeaderCell>Monto</Table.TextHeaderCell>
+          {
+            Object.keys(data[0]).map((key, index) => (
+              <Table.TextHeaderCell key={index}>
+                {key}
+              </Table.TextHeaderCell>
+            ))
+          }
           <Table.TextHeaderCell
             flexBasis={50}
             flexShrink={0}
@@ -50,10 +28,13 @@ function TableDataComponent(props) {
         <Table.Body height={240}>
           {data.map((data, index) => (
             <Table.Row key={index}>
-              <Table.TextCell>{data.fecha}</Table.TextCell>
-              <Table.TextCell>{data.concepto}</Table.TextCell>
-              <Table.TextCell>{data.categoria}</Table.TextCell>
-              <Table.TextCell isNumber>{data.monto}</Table.TextCell>
+              {
+                Object.values(data).map((value, index) => (
+                  <Table.TextCell key={index}>
+                    {value}
+                  </Table.TextCell>
+                ))
+              }
               <Table.TextCell flexBasis={50} flexShrink={0} flexGrow={0}>
                 <div className="edit-button" onClick={()=>{console.log("Se clickeo el edit: ", data);}}>
                   <AiOutlineEdit />
