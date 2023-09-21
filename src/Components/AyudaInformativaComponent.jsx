d546a8a0fd5e2ea1ad8efd8f37847671f179fbf9
Angel Garcia <angel_012912@hotmail.com>
2023-09-20 18:42:35 -0600

    Componente de ayuda informativa para fecha

--- src/Components/AyudaInformativaComponent.jsx
+++ src/Components/AyudaInformativaComponent.jsx
@@ -5,30 +5,45 @@ import { CiCircleInfo } from "react-icons/ci";
 
 function AyudaInformativaComponent(props) {
   const [isShown, setIsShown] = React.useState(false);
-  const { data } = props;
+  const { data, title } = props;
 
   const exampleData = () => {
     return (
+      <div>
         <div>
-            <div> <img src="https://img.freepik.com/fotos-premium/vista-superior-reunion-colegas-negocios-intercambiar-ideas-sobre-nuevo-plan-estrategia-comercial-proyecto-analisis-documentos-datos-concepto-finanzas-contabilidad-reunion-informativa-consultoria-lluvia-ideas_1645-484.jpg?w=2000" alt="" width="95%" /> </div>
-            <h1>Example Data</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda ex eligendi harum, quia quibusdam magni aut accusantium eos cum! Debitis quo magni aperiam, repellat eligendi rerum blanditiis. Assumenda, ab rerum!</p>
+          {" "}
+          <img
+            src="https://img.freepik.com/fotos-premium/vista-superior-reunion-colegas-negocios-intercambiar-ideas-sobre-nuevo-plan-estrategia-comercial-proyecto-analisis-documentos-datos-concepto-finanzas-contabilidad-reunion-informativa-consultoria-lluvia-ideas_1645-484.jpg?w=2000"
+            alt=""
+            width="95%"
+          />{" "}
         </div>
-    )
-  }
+        <h1>Example Data</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda ex
+          eligendi harum, quia quibusdam magni aut accusantium eos cum! Debitis
+          quo magni aperiam, repellat eligendi rerum blanditiis. Assumenda, ab
+          rerum!
+        </p>
+      </div>
+    );
+  };
 
   return (
     <Fragment>
       <Pane>
         <Dialog
           isShown={isShown}
-          title="Dialog title"
+          title={title || "Dialog title"}
           onCloseComplete={() => setIsShown(false)}
           confirmLabel="Custom Label"
           preventBodyScrolling
-          footer = {
-            <div className="ayuda-informativa-button" onClick={()=> setIsShown(false)}>
-                Entendido
+          footer={
+            <div
+              className="ayuda-informativa-button"
+              onClick={() => setIsShown(false)}
+            >
+              Entendido
             </div>
           }
         >
@@ -36,7 +51,11 @@ function AyudaInformativaComponent(props) {
             {data || exampleData()}
           </Pane>
         </Dialog>
-        <div className="ayuda-informativa-main-button" onClick={() => setIsShown(true)} style={{ cursor: "pointer" }}>
+        <div
+          className="ayuda-informativa-main-button"
+          onClick={() => setIsShown(true)}
+          style={{ cursor: "pointer" }}
+        >
           <CiCircleInfo />
         </div>
       </Pane>
--- src/Components/NewRegisterComponent.jsx
+++ src/Components/NewRegisterComponent.jsx
@@ -1,6 +1,7 @@
 import React, { Fragment, useState } from "react";
 import NewRegisterStyle from "../Styles/NewRegisterStyle.css";
 import AyudaInformativaComponent from "./AyudaInformativaComponent";
+import FechaAyudaInformativa from "./FechaAyudaInformativa";
 
 import { TextInputField } from "evergreen-ui";
 
@@ -14,7 +15,7 @@ function NewRegisterComponent(props) {
           <TextInputField
             isInvalid={false}
             type="date"
-            label= {<div className="new-register-title"> <AyudaInformativaComponent /> Fecha</div>}
+            label= {<div className="new-register-title"> <AyudaInformativaComponent data={<FechaAyudaInformativa/>} title={"Importancia de la fecha en registros financieros"} /> Fecha</div>}
           />
           <TextInputField
             isInvalid={false}
